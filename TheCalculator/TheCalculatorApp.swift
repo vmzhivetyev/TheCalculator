0049e945b103309e1311f8c6ee715b9399c02202
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2023-05-29 03:36:00 +0200

    Initial Commit

---
+++ TheCalculator/TheCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheCalculatorApp.swift
+//  TheCalculator
+//
+//  Created by vmzhivetev on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TheCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
